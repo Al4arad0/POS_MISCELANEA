0525ecc99c045a69cfa576e9f07b29a2bbd6bba6
Al4arad0 <137982457+Al4arad0@users.noreply.github.com>
2025-12-05 13:40:32 -0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"; 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_2gMKzZuOtATxu-KIjcsTKq26DMEcXbA",
-  authDomain: "pos-miscelanea.firebaseapp.com",
-  projectId: "pos-miscelanea",
-  storageBucket: "pos-miscelanea.firebasestorage.app",
-  messagingSenderId: "1078315527152",
-  appId: "1:1078315527152:web:99063a9619e13921a08b35",
-  measurementId: "G-1YJJQV5LVY"
+  apiKey:,
+  authDomain:,
+  projectId:,
+  storageBucket:,
+  messagingSenderId:,
+  appId:,
+  measurementId: 
 };
 
 const app = initializeApp(firebaseConfig);
